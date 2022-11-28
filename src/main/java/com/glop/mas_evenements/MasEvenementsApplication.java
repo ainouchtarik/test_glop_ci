6519335ada741f66177a6fd16a38e4941acb8029
Ainouch Tarik <tarik.ainouch.etu@b12p3.fil.univ-lille.fr>
2022-11-28 08:39:34 +0100

    test

---
+++ src/main/java/com/glop/mas_evenements/MasEvenementsApplication.java
@@ -0,0 +1,13 @@
+package com.glop.mas_evenements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasEvenementsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasEvenementsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/glop/mas_evenements/MasEvenementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glop.mas_evenements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasEvenementsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
